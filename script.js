cd78fde3b02ae10cc63773047e6a73249a8acb7d
pal-technologies1 <pfolorunso7@gmail.com>
2025-11-14 17:41:19 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// ====== Mobile Menu Toggle ======
+const menuToggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+menuToggle.addEventListener('click', () => {
+  navLinks.classList.toggle('show');
+});
+
